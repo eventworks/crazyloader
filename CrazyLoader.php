9b5eb31b1fa65d9e166fdf7413d7f1548dcbdec1
Eric Abruzzese <eabruzzese@eventworks.org>
2012-01-07 05:51:04 -0500

    Update to allow for (un)serialization of mapping data

--- CrazyLoader.php
+++ CrazyLoader.php
@@ -1,14 +1,34 @@
 <?php
 
+namespace CrazyLoader;
+
+class ClassNotFoundException
+{
+  public function __construct($message)
+  {
+    echo $message;
+    error_log($message);
+  }
+}
+
 class CrazyLoader {
 
   private $classMap;
+  private $cacheFile;
 
-  public function __construct() {}
-
-  public function getClassMap()
+  public function __construct($cacheFile = 'classMap.cache', $recompile = false)
   {
-    return $this->classMap;
+    $this->cacheFile = $cacheFile;
+
+    if($recompile || !file_exists($this->cacheFile))
+    {
+      @unlink($this->cacheFile);
+      $this->compile();
+    } else {
+      $this->classMap = unserialize(file_get_contents($this->cacheFile));
+    }
+
+    spl_autoload_register(array($this, 'loadClass'));
   }
 
   /**
@@ -25,9 +45,9 @@ class CrazyLoader {
    */
   public function compile()
   {
-    $directoryIterator = new RecursiveDirectoryIterator(realpath(__DIR__));
-    $fileIterator = new RecursiveIteratorIterator($directoryIterator, RecursiveIteratorIterator::SELF_FIRST);
-    $regexIterator = new RegexIterator($fileIterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+    $directoryIterator = new \RecursiveDirectoryIterator(realpath(__DIR__));
+    $fileIterator = new \RecursiveIteratorIterator($directoryIterator, \RecursiveIteratorIterator::SELF_FIRST);
+    $regexIterator = new \RegexIterator($fileIterator, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH);
 
     foreach($regexIterator as $filePath => $fileObject){
       $sourceCode = file_get_contents($filePath);
@@ -54,13 +74,39 @@ class CrazyLoader {
           {
             $className .= $namespace . '\\' . $tokens[$key][1];
             $classNames[] = $className;
+            $className = '';
           }
         }
       }
       $this->addMapping($filePath, $classNames);
     }
+    file_put_contents($this->cacheFile, serialize($this->getClassMap()));
   }
 
+  /**
+   * getClassMap() : array
+   *
+   * Returns the Class->File mappings as an array.
+   *
+   * @return array $classMap
+   */
+  public function getClassMap()
+  {
+    return $this->classMap;
+  }
+
+  /**
+   * addMapping() : void
+   *
+   * Given a filename and an array of class names (namespaces included),
+   * creates a mapping between those classes and a given file name.
+   *
+   * @param string $filePath
+   *    The realpath to a given PHP file
+   * @param array $classNames
+   *    A set of class names (including namespaces) contained within
+   *    the file at $filePath
+   */
   public function addMapping($filePath, $classNames)
   {
     foreach($classNames as $className)
@@ -68,9 +114,26 @@ class CrazyLoader {
       $this->classMap[$className] = $filePath;
     }
   }
+
+  /**
+   * loadClass() : void
+   *
+   * Includes a required file given a class name.
+   *
+   * @param string $className
+   *    A fully-qualified class name.
+   */
+  public function loadClass($className)
+  {
+    if(!isset($this->classMap[$className]))
+    {
+      throw new ClassNotFoundException("The required class $className could not be loaded.");
+    }
+
+    require_once($this->classMap[$className]);
+  }
 }
 
 $autoload = new CrazyLoader();
-$autoload->compile();
 
 print_r($autoload->getClassMap());
